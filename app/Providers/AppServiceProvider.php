de7067e8bd2159540f29a1d9decc59cfef556ae2
phprookiehbb <646054215@qq.com>
2019-04-23 14:48:36 +0800

    Ð޸Ä

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Config;
 use App\Models\Nav;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+     	 Schema::defaultStringLength(191);
         view()->composer('home.layout.header', function ($view){
             $navbars = Nav::orderBy('sort','asc')->get()->toTree();
             $view->with('navbars', $navbars);
