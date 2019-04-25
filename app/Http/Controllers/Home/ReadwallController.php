55abdf26b98269016875d9c0f65c379011de4416
phprookiehbb <646054215@qq.com>
2019-04-25 16:19:29 +0800

    修改readwall

--- app/Http/Controllers/Home/ReadwallController.php
+++ app/Http/Controllers/Home/ReadwallController.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Services\CommentService;
 use App\Models\User;
 
-class ReadWallController extends Controller
+class ReadwallController extends Controller
 {
     public function index()
     {
